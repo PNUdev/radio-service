779c196fcc479ad906c0cca080592e33bdda829e
Igor-Polatajko <igor.bogdanovich39@gmail.com>
2020-06-14 22:54:04 +0300

    Inital commit

---
+++ src/main/java/com/pnu/dev/radioserviceapi/RadioServiceApiApplication.java
@@ -0,0 +1,13 @@
+package com.pnu.dev.radioserviceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RadioServiceApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RadioServiceApiApplication.class, args);
+    }
+
+}
