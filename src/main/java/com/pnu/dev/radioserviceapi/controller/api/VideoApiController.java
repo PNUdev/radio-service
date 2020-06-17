7778e8b2225656231c364fa184f7a93204935220
Igor-Polatajko <igor.bogdanovich39@gmail.com>
2020-06-17 21:36:26 +0300

    Fixed description

--- src/main/java/com/pnu/dev/radioserviceapi/controller/api/VideoApiController.java
+++ src/main/java/com/pnu/dev/radioserviceapi/controller/api/VideoApiController.java
@@ -8,8 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class VideoApiController {
 
     // DTO with :
-    // 1. recent - list of recent videos
-    // 2. recommended - list of recommended videos (call out to youtube api)
+    // 1. recent - list of recent videos (call out to youtube api)
+    // 2. recommended - list of recommended videos
     // 3. planned or current streams
 
 }
