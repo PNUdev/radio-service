e04335e456282a2274635956e54358ad37dc59c6
Ihor Polataiko <igor.bogdanovich39@gmail.com>
2020-09-13 00:47:58 +0300

    Removed redundant controller

--- radio-service-api/src/main/java/com/pnu/dev/radioserviceapi/config/WebConfig.java
+++ radio-service-api/src/main/java/com/pnu/dev/radioserviceapi/config/WebConfig.java
@@ -4,13 +4,19 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.resource.PathResourceResolver;
 
 import java.io.IOException;
 
 @Configuration
-public class StaticConfig implements WebMvcConfigurer {
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/admin").setViewName("adminWelcomePage");
+    }
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
