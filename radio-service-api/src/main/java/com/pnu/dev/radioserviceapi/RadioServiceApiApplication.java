96219f535d88807e9d83efdc971c7d06c4c927da
Ihor Polataiko <34420901+Igor-Polatajko@users.noreply.github.com>
2020-10-24 18:40:52 +0300

    Removed scheduled job (#53)

--- radio-service-api/src/main/java/com/pnu/dev/radioserviceapi/RadioServiceApiApplication.java
+++ radio-service-api/src/main/java/com/pnu/dev/radioserviceapi/RadioServiceApiApplication.java
@@ -2,12 +2,10 @@ package com.pnu.dev.radioserviceapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 @EnableWebMvc
-@EnableScheduling
 public class RadioServiceApiApplication {
 
     public static void main(String[] args) {
