24747bee4c384230b9ab745415cbbd05181b1c4e
Ihor Polataiko <igor.bogdanovich39@gmail.com>
2021-01-16 13:40:04 +0200

    [HOTFIX] Fix Youtube API call error

--- radio-service-api/src/main/java/com/pnu/dev/radioserviceapi/client/dto/search/YoutubeSearchResponse.java
+++ radio-service-api/src/main/java/com/pnu/dev/radioserviceapi/client/dto/search/YoutubeSearchResponse.java
@@ -1,12 +1,16 @@
 package com.pnu.dev.radioserviceapi.client.dto.search;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class YoutubeSearchResponse {
 
     private List<ItemYoutubeSearchResponse> items;
